5049bd85f9a99756a99bdcadc1ad96611d97ddfb
kuzzaka <kuzzaka@gmail.com>
2016-12-04 19:56:55 +0500

    add images add json-server add services.js

---
+++ www/js/services.js
@@ -0,0 +1,32 @@
+'use strict';
+
+angular.module('confusion.services', ['ngResource'])
+    .constant('baseUrl', 'http://localhost:3000/')
+    .service('menuFactory', ['$resource', 'baseUrl', function($resource, baseUrl) {
+
+      this.getDishes = function() {
+        return $resource(baseUrl + 'dishes/:id', null, {'update': {method: 'PUT'}});
+      };
+
+      this.getPromotions = function() {
+        return $resource(baseUrl + 'promotions/:id');
+      }
+    }])
+
+    .factory('corporateFactory', ['$resource', 'baseUrl', function($resource, baseUrl) {
+
+      var corpfac = {};
+
+      corpfac.getLeaders = function() {
+        return $resource(baseUrl + 'leadership/:id');
+      };
+
+      return corpfac;
+    }])
+
+    .service('feedbackFactory', ['$resource', 'baseUrl', function($resource, baseUrl) {
+      this.getFeedback = function() {
+        return $resource(baseUrl + 'feedback/:id');
+      }
+    }])
+;
